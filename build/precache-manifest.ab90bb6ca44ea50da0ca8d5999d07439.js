c43bc81efcf6e6ef1de1c3cc8ef723164bbd66c4
arunkumarkalai1398 <57340179+arunkumarkalai1398@users.noreply.github.com>
2020-10-14 19:53:59 +0530

    Add files via upload

---
+++ build/precache-manifest.ab90bb6ca44ea50da0ca8d5999d07439.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2159046957437500d7ec4a1e00155099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a518acd168d576a31f6",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "55db38d87e7576949dbb",
+    "url": "/static/css/main.be464536.chunk.css"
+  },
+  {
+    "revision": "3a518acd168d576a31f6",
+    "url": "/static/js/2.c02ef360.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c02ef360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55db38d87e7576949dbb",
+    "url": "/static/js/main.6b9c441f.chunk.js"
+  },
+  {
+    "revision": "806c31e00546d93ba6aa",
+    "url": "/static/js/runtime-main.cf7ffcd5.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/static/media/bg-header-desktop.6e2f7196.svg"
+  }
+]);
